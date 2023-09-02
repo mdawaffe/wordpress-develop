8963c7e32ad642be6cae9fa2a36fcfbbb62acb90
Michael D Adams <michael.d.adams@gmail.com>
2023-09-02 12:21:48 -0700

    try DiD again - start with no WP just to play with things

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,16 +3,16 @@
 set -eux
 
 if [ -z ${CODESPACE_NAME+x} ]; then
-	SITE_HOST="http://localhost:8080"
+	SITE_HOST="http://localhost:8889"
 else
-	SITE_HOST="https://${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+	SITE_HOST="https://${CODESPACE_NAME}-8889.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 fi
 
 # Install dependencies
-cd /workspaces/wordpress-develop
-npm install && npm run build:dev
+#cd /workspaces/wordpress-develop
+#npm install && npm run build:dev
 
 # Install WordPress and activate the plugin/theme.
-cd /var/www/html
-echo "Setting up WordPress at $SITE_HOST"
-wp core install --url="$SITE_HOST" --title="WordPress Trunk" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
+#cd /var/www/html
+#echo "Setting up WordPress at $SITE_HOST"
+#wp core install --url="$SITE_HOST" --title="WordPress Trunk" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
