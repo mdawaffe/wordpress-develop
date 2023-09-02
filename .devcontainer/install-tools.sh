aab9aaa87d98fe8028bb5359d53fc0a957c68a56
Michael D Adams <michael.d.adams@gmail.com>
2023-09-02 12:49:04 -0700

    we don't need wpcli - we use the container

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -2,11 +2,6 @@
 
 set -eux
 
-#echo "Installing wp-cli..."
-#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#sudo chmod +x wp-cli.phar
-#sudo mv wp-cli.phar /usr/local/bin/wp
-
 #echo "Installing chromium..."
 #sudo apt-get update
 #sudo apt-get -y install --no-install-recommends chromium
