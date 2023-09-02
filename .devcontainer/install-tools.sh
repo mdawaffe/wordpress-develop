cb13d30b05365e24b7b9610600f7ffac5d7abee0
Michael D Adams <michael.d.adams@gmail.com>
2023-09-02 12:52:20 -0700

    Add URL to welcome message, along with auth
    Props @rmccue

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -4,3 +4,5 @@ set -eux
 
 # Copy the welcome message
 sudo cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+SITE_URL="https://${CODESPACE_NAME}-8889.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+sudo sed -i "s!SITE_URL!$SITE_URL!" /usr/local/etc/vscode-dev-containers/first-run-notice.txt
