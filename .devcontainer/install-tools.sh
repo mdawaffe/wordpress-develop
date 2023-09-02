bdb8e0ff439037e11296e37501634b5c2c0da3ea
Michael D Adams <michael.d.adams@gmail.com>
2023-09-02 12:50:04 -0700

    Remove Chromium install
    This is very slow on the platforms it works on, but also is not
    available on our new devcontainer at all. Users can manually install
    this for e2e testing where necessary.
    
    Props @rmccue

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -2,9 +2,5 @@
 
 set -eux
 
-#echo "Installing chromium..."
-#sudo apt-get update
-#sudo apt-get -y install --no-install-recommends chromium
-
 # Copy the welcome message
 sudo cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
